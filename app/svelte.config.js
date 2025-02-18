fb0476632f48bc526e4b15a4055521b28e6163d1
nofurtherinformation <dhalpern@uchicago.edu>
2025-02-18 11:49:16 -0600

    build fixes

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -13,7 +13,11 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '200.html' // may differ from host to host
-		})
+			
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/core-facility-data-mirror' : '',
+	}
 	}
 };
 
